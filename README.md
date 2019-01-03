264243f8772cabdc5bcaafaf6928f934b5d5b182
lispmeister <markus.fix@keep.network>
2019-01-03 19:51:02 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+collectd-bitcoin.py
+======
+Python plugin for collectd to query bitcoind via rpc.
+
+This is based on a python script from C-Otto found at:
+https://gist.github.com/C-Otto/a9e4864dff1a2b167761
+
+This is also based on a python script by nomnombtc:
+https://github.com/nomnombtc/collectd-bitcoin 
+
+Mostly changes to make the query code work within the collectd
+plugin environment. 
+
+Together with something like [CGP](https://github.com/pommi/CGP) it can generate nice graphs:
+![Screenshot of CGP Graphpanel](collectd-bitcoin.png)
+(Note if you use CGP, there is a custom plugin to control colors/naming scheme in [the CGP/ subfolder](CGP/).)
+
+Usage
+======
+
+Needs a running bitcoin-{core,classic,unlimited} daemon with rpc user/password set.
+
+This is needed to connect via rpc: https://github.com/jgarzik/python-bitcoinrpc
+
+Replace ```rpcuser``` and ```rpcpassword``` with your bitcoind rpc username/password in the script.
+
+Enable collectd Python plugin:
+```
+LoadPlugin python
+```
+
+Put something like this into collectd.conf assuming that your plugins
+live in `/opt/collectd_plugins`:
+```
+<Plugin python>
+    ModulePath "/opt/collectd_plugins"
+    Import "collectd-bitcoin-plugin"
+</Plugin>
+```
---
+++ collectd-bitcoin-plugin.py
@@ -0,0 +1,174 @@
+from bitcoinrpc.authproxy import AuthServiceProxy
+import collectd
+import sys, traceback
+
+
+RPC_USER = 'change_me'
+RPC_PASSWORD = 'change_me'
+BITCOIN_SERVER = '127.0.0.1:8332'
+AUTHSERV = 'http://' + RPC_USER + ':' + RPC_PASSWORD + '@' + BITCOIN_SERVER
+
+
+def new_proxy():
+    """
+    Creates and returns a new proxy object that talks to the specified bitcoin
+    node. This is necessary because the Python plugin for collectd is heavily
+    multithreaded and each of the defined entrypoints can be called con-
+    currently and needs its own proxy.
+    """
+    return AuthServiceProxy(AUTHSERV)
+
+
+def read_nettotals():
+    proxy = new_proxy()
+    response = proxy.getnettotals()
+    funcname = str('getnettotals')
+    for subkey in ['totalbytesrecv', 'totalbytessent']:
+        total_bytes = response[subkey]
+        val = collectd.Values(type='counter',
+                              host='',
+                              plugin='bitcoind',
+                              time=0)
+        val.plugin_instance = funcname
+        val.type_instance = subkey
+        val.dispatch(values=[total_bytes])
+
+
+def read_networkinfo():
+    proxy = new_proxy()
+    response = proxy.getnetworkinfo()
+    funcname = str('getnetworkinfo')
+    subkey = str('connections')
+    number_of_connections = response[subkey]
+    val = collectd.Values(type='gauge',
+                          host='',
+                          plugin='bitcoind',
+                          time=0)
+    val.plugin_instance = funcname
+    val.type_instance = subkey
+    val.dispatch(values=[number_of_connections])
+
+
+def read_mempoolinfo():
+    proxy = new_proxy()
+    response = proxy.getmempoolinfo()
+    funcname = str('getmempoolinfo')
+    for subkey in ['size', 'bytes']:
+        funccat = (str(funcname) + '_' + str(subkey))
+        info = response[subkey]
+        val = collectd.Values(type='gauge',
+                              host='',
+                              plugin='bitcoind',
+                              time=0)
+        val.plugin_instance = funccat
+        val.type_instance = subkey
+        val.dispatch(values=[info])
+
+
+def read_estimatesmartfee():
+    # Starting with v0.17 only estimatesmartfee call is supported.
+    # Estimate cost to include the transaction within 6 blocks.
+    # We convert this value to Satoshis to make it an integer
+    proxy = new_proxy()
+    response = proxy.estimatesmartfee(6)
+    funcname = str('estimatesmartfee')
+    feerate = response['feerate']
+    feerate_in_satoshi = feerate * 100000000
+    val = collectd.Values(type='gauge',
+                          host='',
+                          plugin='bitcoind',
+                          time=0)
+    val.plugin_instance = funcname
+    val.type_instance = str('Satoshi')
+    val.dispatch(values=[feerate_in_satoshi])
+
+
+def read_blockcount():
+    proxy = new_proxy()
+    # get size, height, diff of the last block
+    response = proxy.getblockcount()
+    blockcount = response
+    # get hash of last block
+    response = proxy.getblockhash(blockcount)
+    blockhash = response
+    # get info from blockhash
+    response = proxy.getblock(blockhash)
+    funcname = str('getblock')
+    for subkey in ['size', 'height', 'difficulty']:
+        funccat = (str(funcname) + '_' + str(subkey))
+        value = response[subkey]
+        val = collectd.Values(type='gauge',
+                              host='',
+                              plugin='bitcoind',
+                              time=0)
+        val.plugin_instance = funccat
+        val.type_instance = subkey
+        val.dispatch(values=[value])
+
+
+def read_networkhashps():
+    # network hashrate
+    proxy = new_proxy()
+    response = proxy.getnetworkhashps()
+    funcname = str('getnetworkhashps')
+    networkhashps = response
+    val = collectd.Values(type='gauge',
+                          host='',
+                          plugin='bitcoind',
+                          time=0)
+    val.plugin_instance = funcname
+    val.type_instance = funcname
+    val.dispatch(values=[networkhashps])
+
+
+def read_func():
+    try:
+        read_nettotals()
+    except Exception:
+        collectd.error('bitcoin plugin: exception reading nettotals value!')
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        collectd.error(repr(traceback.format_exception(exc_type, exc_value,
+                                                       exc_traceback)))
+
+    try:
+        read_networkinfo()
+    except Exception:
+        collectd.error('bitcoin plugin: exception reading networkinfo!')
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        collectd.error(repr(traceback.format_exception(exc_type, exc_value,
+                                                       exc_traceback)))
+
+    try:
+        read_mempoolinfo()
+    except Exception:
+        collectd.error('bitcoin plugin: exception reading mempoolinfo!')
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        collectd.error(repr(traceback.format_exception(exc_type, exc_value,
+                                                       exc_traceback)))
+
+    try:
+        read_estimatesmartfee()
+    except Exception:
+        collectd.error('bitcoin plugin: exception reading estimatesmartfee!')
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        collectd.error(repr(traceback.format_exception(exc_type, exc_value,
+                                                       exc_traceback)))
+
+    try:
+        read_blockcount()
+    except Exception:
+        collectd.error('bitcoin plugin: exception reading blockcount!')
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        collectd.error(repr(traceback.format_exception(exc_type, exc_value,
+                                                       exc_traceback)))
+
+    try:
+        read_networkhashps()
+    except Exception:
+        collectd.error('bitcoin plugin: exception reading networkhasps!')
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        collectd.error(repr(traceback.format_exception(exc_type, exc_value,
+                                                       exc_traceback)))
+
+
+collectd.register_read(read_func)
